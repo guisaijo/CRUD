ccdb6d0715d4e109da9e774797e285dd61b247dc
guisaijo <guilherme.saijo@rivendel.com.br>
2018-03-27 11:26:34 -0300

    dlg

--- src/main/java/beans/UserView.java
+++ src/main/java/beans/UserView.java
@@ -11,9 +11,9 @@ import javax.faces.bean.ViewScoped;
 
 import domain.User;
  
-@ManagedBean(name="user_view")
+@ManagedBean(name="userView")
 @ViewScoped
-public class user_view implements Serializable {
+public class UserView implements Serializable {
 	
 	/**
 	 * 
@@ -21,8 +21,8 @@ public class user_view implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private List<User> users;
-	private List<User> filtered_users;
-	private User selected_user;
+	private List<User> filteredUsers;
+	private User selectedUser;
  
 	@PostConstruct
     public void init() {
@@ -89,22 +89,20 @@ public class user_view implements Serializable {
         return users;
     }
 
-	public List<User> getFiltered_users() {
-		return filtered_users;
+	public List<User> getFilteredUsers() {
+		return filteredUsers;
 	}
 
-	public void setFiltered_users(List<User> filtered_users) {
-		this.filtered_users = filtered_users;
+	public void setFilteredUsers(List<User> filteredUsers) {
+		this.filteredUsers = filteredUsers;
 	}
 
-	public User getSelected_user() {
-		return selected_user;
+	public User getSelectedUser() {
+		return selectedUser;
 	}
 
-	public void setSelected_user(User selected_user) {
-		this.selected_user = selected_user;
-	}
-    
-    
+	public void setSelectedUser(User selectedUser) {
+		this.selectedUser = selectedUser;
+	}    
 }
 
